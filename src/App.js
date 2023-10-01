505f2686fc8597f06a52b47e3d73d6370445db90
0w00ng <k24179906@gmail.com>
2023-10-02 01:36:42 +0900

    topbar 디자인 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div class="page">
       <div class="topbar">
-        <div class="topbar-text">로아경제</div>
+        <div class="topbar-inline">
+          <div class="topbar-text">로아경제</div>
+        </div>
       </div>
       <div>
         <div class="body"></div>
